542706b2aa4358dad523fe5252e297158aa8589a
Alex Yang <aleozlx@gmail.com>
2017-10-31 02:24:46 +0000

    changed to bfs

--- main.py
+++ main.py
@@ -1,22 +1,49 @@
+import os, sys
 import numpy as np
+from collections import deque, OrderedDict
+import unittest, random, itertools
 
-a = np.arange(4)
-u = lambda x: x[[0, 2, 3, 1]]
-v = lambda x: x[[3, 1, 0, 2]]
-y = lambda x: x[[1, 3, 2, 0]]
-z = lambda x: x[[2, 0, 1, 3]]
-print(u(a))
+class TetrahedronOrientation(tuple):
+    actions = OrderedDict([
+        ('u', [0, 2, 3, 1]), ('u2', [0, 3, 1, 2]),
+        ('v', [3, 1, 0, 2]), ('v2', [2, 1, 3, 0]),
+        ('y', [1, 3, 2, 0]), ('y2', [3, 0, 2, 1]),
+        ('z', [2, 0, 1, 3]), ('z2', [1, 2, 0, 3])
+    ])
 
-def dfs(G, a = None):
-    visited = np.zeros_like(G).astype(bool)
-    predecessors = -np.ones_like(G)
-    stack = [a]
-    while stack:
-        u = stack.pop()
-        if not visited[u]:
-            visited[u] = True
-            if G[u]:
-                discovered = np.array([[v, next(counter)] for v in G[u] if not visited[v]])
-                stack.extend(reversed(discovered[:, 0]))
-                predecessors[discovered[:, 0]] = u
-    return predecessors
+    def permutate(self, order):
+        return TetrahedronOrientation(np.array(self)[order])
+        
+    def rotate(self, direction):
+        return self.permutate(TetrahedronOrientation.actions[direction])
+        
+    def rotations(self):
+        for action in TetrahedronOrientation.actions:
+            yield self.rotate(action)
+
+a = TetrahedronOrientation(np.arange(4))
+
+def bfs_tetrahedron_symmetry(s):
+    counter = itertools.count(1)
+    visited = set()
+    discover = dict()
+    predecessor = dict()
+    results = (discover, predecessor)
+
+    queue = deque([s])
+    discover[s] = next(counter)
+
+    while queue:
+        u = queue.popleft()
+        if u not in visited:
+            visited.add(u)
+            discovered = [[v, next(counter)] for v in u.rotations() if v not in visited]
+            queue.extend(v for v,dv in discovered)
+            discover.update({v:dv for v,dv in discovered})
+            for v,dv in discovered:
+                predecessor[v] = u
+
+    return results
+
+tetrahedron_symmetry_discovery, tetrahedron_symmetry_predecessor = bfs_tetrahedron_symmetry(a)
+print(len(tetrahedron_symmetry_discovery))
